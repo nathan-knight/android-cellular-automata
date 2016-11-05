97c50b43b85275ce5ebd092d39edc16ad7f33407
thee-code-warrior <knathanknight@gmail.com>
2016-11-05 17:59:18 -0400

    Before changes

--- app/src/main/java/codes/knight/cellularautomata/LifeActivity.java
+++ app/src/main/java/codes/knight/cellularautomata/LifeActivity.java
@@ -1,13 +1,42 @@
 package codes.knight.cellularautomata;
 
-import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by thee-code-warrior on 9/22/2016.
+ */
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class LifeActivity extends AppCompatActivity {
+
+    private LifeView surfaceView;
+    private Thread gameThread;
+
+    private boolean running = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life);
+
+        surfaceView = (LifeView) findViewById(R.id.surfaceView);
+
+        gameThread = new Thread(surfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!gameThread.isAlive()) {
+            gameThread.run();
+        } else {
+            surfaceView.setRunning(false);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        surfaceView.setRunning(false);
     }
 }
--- app/src/main/java/codes/knight/cellularautomata/LifeView.java
+++ app/src/main/java/codes/knight/cellularautomata/LifeView.java
@@ -1,7 +1,78 @@
 package codes.knight.cellularautomata;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 /**
- * Created by Nathan on 9/22/2016.
+ * Created by thee-code-warrior on 9/22/2016.
  */
-public class LifeView {
+public class LifeView extends SurfaceView implements Runnable, SurfaceHolder.Callback {
+
+    private boolean running = true;
+    Surface surface;
+
+    public LifeView(Context context) {
+        super(context);
+    }
+
+    public LifeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void run() {
+        Canvas canvas = null;
+        SurfaceHolder holder = getHolder();
+        while (true) {
+            if(running) {
+                draw();
+            }
+        }
+    }
+
+    private void draw() {
+        if(surface == null) surface = getHolder().getSurface();
+        Canvas canvas;
+        Log.d("LifeActivity", "DRAWING?!");
+        if(surface.isValid()) {
+            canvas = surface.lockCanvas(null);
+            canvas.drawColor(Color.argb(255, 100, 200, 100));
+            canvas.drawText("TEST", 100, 100, new Paint());
+            surface.unlockCanvasAndPost(canvas);
+        }
+    }
+
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//        canvas.drawText("Hello world!", 20, 20, new Paint());
+//        Log.d("LifeActivity", "DRAWING?!");
+////        gameController.draw(canvas);
+//    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+//        setWillNotDraw(false);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
 }
